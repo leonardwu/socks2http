618214036c548861a2597cfa2060ce2a97249050
Leonard Woo <leo_916@yahoo.com>
2018-12-16 19:42:38 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module socks2http
+
+require golang.org/x/net v0.0.0-20181213202711-891ebc4b82d6
--- main.go
+++ main.go
@@ -2,20 +2,74 @@ package main
 
 import (
 	"flag"
+	"golang.org/x/net/proxy"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
-
-	"github.com/Evan2698/socks2http/core"
-	"golang.org/x/net/proxy"
 )
 
+var (
+	laddr = flag.String("laddr", "0.0.0.0:8180", "Address to listen on")
+	raddr = flag.String("raddr", "socks5://127.0.0.1:1080", "Socks proxy address to connect to.")
+)
+
+type HttpProxyRoutineHandler struct {
+	Dialer proxy.Dialer
+}
+
+func (h *HttpProxyRoutineHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	hijack, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "webserver doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+
+	port := r.URL.Port()
+	if port == "" {
+		port = "80"
+	}
+	socksConn, err := h.Dialer.Dial("tcp", r.URL.Hostname()+":"+port)
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	defer socksConn.Close()
+	httpConn, _, err := hijack.Hijack()
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer httpConn.Close()
+	if r.Method == http.MethodConnect {
+		httpConn.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	} else {
+		r.Write(socksConn)
+	}
+
+	msgRead := make(chan string)
+	msgWrite := make(chan string)
+	pipeConn := func(w, r net.Conn, ch chan<- string) {
+		io.Copy(w, r)
+		ch <- "done!"
+	}
+
+	go pipeConn(socksConn, httpConn, msgRead)
+	go pipeConn(httpConn, socksConn, msgWrite)
+
+	<-msgRead
+	<-msgWrite
+}
+
 func main() {
-	httpAddr := flag.String("http", "0.0.0.0:8008", "local http proxy address")
-	socks5Addr := flag.String("socks5", "socks5://127.0.0.1:1080", "remote socks5 address")
 	flag.Parse()
 
-	socksURL, err := url.Parse(*socks5Addr)
+	socksURL, err := url.Parse(*raddr)
 	if err != nil {
 		log.Fatalln("proxy url parse error:", err)
 	}
@@ -23,8 +77,7 @@ func main() {
 	if err != nil {
 		log.Fatalln("can not make proxy dialer:", err)
 	}
-	if err := http.ListenAndServe(*httpAddr, &core.HttpProxyRoutineHandler{Dialer: socks5Dialer}); err != nil {
+	if err := http.ListenAndServe(*laddr, &HttpProxyRoutineHandler{Dialer: socks5Dialer}); err != nil {
 		log.Fatalln("can not start http server:", err)
 	}
-
 }
